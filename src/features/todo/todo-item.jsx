b9f2ac4764fe439653a9cb0138b28d9a6790db9d
JunuThapa <junuthapa011@gmail.com>
2023-07-21 00:39:22 -0400

    stopped rerendering of the page

--- src/features/todo/todo-item.jsx
+++ src/features/todo/todo-item.jsx
@@ -1,33 +1,32 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { toggleComplete, deleteTodo } from '../todo/todo-slice'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleComplete, deleteTodo } from "../todo/todo-slice";
 
 const TodoItem = ({ id, title, completed }) => {
-    const dispatch = useDispatch();
-   
-    const handleCompleteClick = () => {
-        dispatch(toggleComplete({ id: id, completed: !completed})
-        );
-    
-    }
-    const handleDeleteClick = () => {
-        dispatch(deleteTodo({id: id}))
-    }
-return (
-    <div className='tasks'>
-        <form onSubmit={handleCompleteClick}>
-            <input 
-            type="checkbox" 
-            className='del' 
-            checked={completed}
-            onChange={handleCompleteClick}>
+  const dispatch = useDispatch();
 
-            </input>
-            {title}
-            <button onClick={handleDeleteClick}>DELETE</button>
-        </form>
+  const handleCompleteClick = (e) => {
+    e.preventDefault();
+    dispatch(toggleComplete({ id: id, completed: !completed }));
+  };
+  const handleDeleteClick = (e) => {
+    e.preventDefault();
+    dispatch(deleteTodo({ id: id }));
+  };
+  return (
+    <div className="tasks">
+      <form onSubmit={handleCompleteClick}>
+        <input
+          type="checkbox"
+          className="del"
+          checked={completed}
+          onChange={handleCompleteClick}
+        ></input>
+        {title}
+        <button onClick={handleDeleteClick}>DELETE</button>
+      </form>
     </div>
   );
-}
+};
 
-export default TodoItem
+export default TodoItem;
